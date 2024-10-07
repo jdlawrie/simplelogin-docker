cb6b5b68860ecf83823d6d836fa892f55b20014c
James Lawrie <james.lawrie@percona.com>
2024-10-07 13:19:35 +0200

    Create init.sh

---
+++ postfix/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILES="localtime services resolv.conf hosts nsswitch.conf"
+for file in $FILES; do
+    cp /etc/${file} /var/spool/postfix/etc/${file}
+    chmod a+rX /var/spool/postfix/etc/${file}
+done
